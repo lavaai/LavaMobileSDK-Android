bb3bafeb058a74e8b486f3a13760092a67a55c08
Thuong Nguyen <93107606+thuongnh-rs@users.noreply.github.com>
2025-12-04 10:40:02 +0700

    fix: handle permissions for Android 13+ (#19)

--- DemoApp/app/src/main/java/ai/lava/demoapp/android/SplashScreenActivity.kt
+++ DemoApp/app/src/main/java/ai/lava/demoapp/android/SplashScreenActivity.kt
@@ -2,17 +2,28 @@ package ai.lava.demoapp.android
 
 import ai.lava.demoapp.android.auth.LoginActivity
 import ai.lava.demoapp.android.utils.CLog
+import android.Manifest
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
 import com.lava.lavasdk.Lava
 
 //import io.fabric.sdk.android.Fabric;
+@SuppressLint("CustomSplashScreen")
 class SplashScreenActivity : Activity() {
+
+  private val REQUEST_CODE = 1001
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     //Fabric.with(this, new Crashlytics());
@@ -22,7 +33,27 @@ class SplashScreenActivity : Activity() {
       hide(WindowInsetsCompat.Type.statusBars() or WindowInsetsCompat.Type.navigationBars())
     }
 
-    launchSuitableActivity()
+    requestNotificationPermission()
+  }
+
+  private fun requestNotificationPermission() {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+      if (ContextCompat.checkSelfPermission(
+          this, 
+          Manifest.permission.POST_NOTIFICATIONS
+        ) != PackageManager.PERMISSION_GRANTED
+      ) {
+        ActivityCompat.requestPermissions(
+          this,
+          arrayOf(Manifest.permission.POST_NOTIFICATIONS),
+          REQUEST_CODE
+        )
+      } else {
+        launchSuitableActivity()
+      }
+    } else {
+      launchSuitableActivity()
+    }
   }
 
   private fun launchSuitableActivity() {
@@ -85,6 +116,15 @@ class SplashScreenActivity : Activity() {
     grantResults: IntArray
   ) {
     super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    if (requestCode == REQUEST_CODE) {
+      if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+        Toast.makeText(this, "Push notification permission granted", Toast.LENGTH_SHORT).show()
+
+      } else {
+        // Permission denied
+        Toast.makeText(this, "Push notification permission denied", Toast.LENGTH_SHORT).show()
+      }
+    }
     launchSuitableActivity()
   }
 
